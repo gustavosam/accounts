e0d2729fbd98297c938c1cb4d56b11fd83c58dd7
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-18 16:12:47 -0500

    El MS Accounts se registra en el servidor de eureka

--- src/main/java/com/microservice/accounts/AccountsApplication.java
+++ src/main/java/com/microservice/accounts/AccountsApplication.java
@@ -2,14 +2,16 @@ package com.microservice.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * La clase principal del microservicio accounts.
  * */
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class AccountsApplication {
 
   public static void main(String[] args) {
